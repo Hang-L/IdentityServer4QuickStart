bf6811f006782f2d6006af95314d006cda658076
行 <liuxing19921203@outlook.com>
2018-12-21 19:34:39 +0900

    添加identityserver4学习

---
+++ IdentityServer4QuickStart/Client/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using IdentityModel;
+using IdentityModel.Client;
+using Newtonsoft.Json.Linq;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MainUpAsync().GetAwaiter().GetResult();
+            Console.ReadLine();
+        }
+
+
+        static async Task MainAsync()
+        {
+            var client = new HttpClient();
+            var dico = await client.GetDiscoveryDocumentAsync("http://localhost:5000");
+
+            var tokenResponse = await client.RequestClientCredentialsTokenAsync(new ClientCredentialsTokenRequest
+            {
+
+                ClientId = "client",
+                ClientSecret = "secret",
+                Scope = "api1",
+                Address = dico.TokenEndpoint
+            });
+            if (tokenResponse.IsError)
+            {
+                Console.WriteLine(tokenResponse.Error);
+                return;
+            }
+            Console.WriteLine(tokenResponse.Json);
+            Console.WriteLine("\n\n");
+
+            client.SetBearerToken(tokenResponse.AccessToken);
+            var response = await client.GetAsync("http://localhost:5001/get");
+            if (response.IsSuccessStatusCode)
+            {
+                Console.WriteLine(response.StatusCode);
+            }
+            else
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(JArray.Parse(content));
+            }
+        }
+
+        static async Task MainUpAsync()
+        {
+            var client = new HttpClient();
+            var disco = await client.GetDiscoveryDocumentAsync("http://localhost:5000");
+            var tokenResponse = await client.RequestPasswordTokenAsync(new PasswordTokenRequest
+            {
+                ClientId="up.client",
+                ClientSecret="secret",
+                Address=disco.TokenEndpoint,
+                Scope="api1",
+                UserName="alice",
+                Password="123456"
+            });
+            if (tokenResponse.IsError)
+            {
+                Console.WriteLine(tokenResponse.Error);
+                return;
+            }
+            Console.WriteLine(tokenResponse.Json);
+
+            client.SetBearerToken(tokenResponse.AccessToken);
+            var response = await client.GetAsync("http://localhost:5001/get");
+            if (response.IsSuccessStatusCode)
+            {
+                Console.WriteLine(response.StatusCode);
+            }
+            else
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(JArray.Parse(content));
+            }
+        }
+    }
+}
